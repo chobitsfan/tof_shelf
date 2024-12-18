bae7a7f2849fcad16ff6e1978ddc6aa5083b857b
chobits tai <chobits@itri.org.tw>
2024-12-19 02:42:48 +0800

    show 3d vertical structure

--- tof_shelf.py
+++ tof_shelf.py
@@ -1,15 +1,20 @@
-import rclpy, cv2
+import rclpy, cv2, math
 import numpy as np
 from rclpy.node import Node
 from sensor_msgs.msg import Image
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Point
 from ArducamDepthCamera import ArducamCamera, TOFConnect, TOFDeviceType, TOFOutput, TOFControl, DepthData, ArducamInfo
 
 GRAD_THRESH = 300
+fx = 240 / (2 * math.tan(0.5 * math.pi * 64.3 / 180));
+fy = 180 / (2 * math.tan(0.5 * math.pi * 50.4 / 180));
 
 rclpy.init()
 node = rclpy.create_node('tof')
-img_pub = node.create_publisher(Image, 'depth_image', 1)
-img_pub2 = node.create_publisher(Image, 'edge_image', 1)
+img_pub = node.create_publisher(Image, "depth_image", 1)
+img_pub2 = node.create_publisher(Image, "edge_image", 1)
+lines_pub = node.create_publisher(Marker, "struct_lines", 1);
 
 tof = ArducamCamera()
 ret = 0
@@ -71,15 +76,41 @@ while rclpy.ok():
                 for line in lines_x_n:
                     l = line[0]
                     cv2.line(edge_img, (l[0], l[1]), (l[2], l[3]), (0,255,0), 1, cv2.LINE_8)
+            # only select vertical lines which postive & negative edges close enough
+            vert_struct = None
+            if lines_x_p is not None and lines_x_n is not None:
+                max_len_sq = 0
+                for line_x_p in lines_x_p:
+                    pl = line_x_p[0]
+                    for line_x_n in lines_x_n:
+                        nl = line_x_n[0]
+                        if pl[0]-nl[0]<20 and pl[0]-nl[0]>2:
+                            dx = pl[0]-pl[2]
+                            dy = pl[1]-pl[3]
+                            len_sq = dx*dx+dy*dy;
+                            if len_sq > max_len_sq:
+                                max_len_sq = len_sq
+                                vert_struct = pl
+                            break
+            #print("vert struct", vert_struct)
 
             # detect horizontal structures
             grad = cv2.Sobel(depth_u16, cv2.CV_16S, 0, 1, -1)
             ret, grad_thresh = cv2.threshold(grad, GRAD_THRESH, 255, cv2.THRESH_BINARY)
             grad_u8 = grad_thresh.astype(np.uint8)
             lines_y = cv2.HoughLinesP(grad_u8, 1, np.pi/180, 50, None, 80, 5)
+            # find the horizontal line with max length
+            hori_struct = None
             if lines_y is not None:
+                max_len_sq = 0
                 for line in lines_y:
                     l = line[0]
+                    dx = l[0]-l[2]
+                    dy = l[1]-l[3]
+                    len_sq = dx*dx+dy*dy
+                    if len_sq > max_len_sq:
+                        max_len_sq = len_sq
+                        hori_struct = l
                     cv2.line(edge_img, (l[0], l[1]), (l[2], l[3]), (255,0,0), 1, cv2.LINE_8)
 
             img.header.stamp = node.get_clock().now().to_msg()
@@ -87,6 +118,52 @@ while rclpy.ok():
             img.step = 240*3
             img.data = edge_img.ravel().view(np.uint8)
             img_pub2.publish(img)
+
+            line_list = Marker()
+            line_list.header.frame_id = "body"
+            line_list.header.stamp = node.get_clock().now().to_msg()
+            line_list.action = Marker.ADD
+            line_list.type = Marker.LINE_LIST
+            line_list.pose.orientation.w = 1
+            line_list.ns = "verti_struct"
+            line_list.scale.x = 0.01
+            line_list.color.r = 1.0
+            line_list.color.a = 1.0
+
+            if vert_struct is not None:
+                v = vert_struct;
+                x1 = v[0]-2
+                x2 = v[2]-2
+                if x1 > 0 and x2 > 0:
+                    if v[1] == 0:
+                        my = 0
+                    elif v[1] == 179:
+                        my = 177
+                    else:
+                        my = v[1] - 1
+                    pp = np.sort(depth_u16[my:my+3, x1-1:x1+2], axis=None)
+                    d = pp[4] * 0.001
+                    p = Point()
+                    p.x = d
+                    p.y = (120 - x1) / fx * d
+                    p.z = (90 - v[1]) / fy * d
+                    line_list.points.append(p)
+
+                    if v[3] == 0:
+                        my = 0
+                    elif v[3] == 179:
+                        my = 177
+                    else:
+                        my = v[3] - 1
+                    pp = np.sort(depth_u16[my:my+3, x2-1:x2+2], axis=None)
+                    d = pp[4] * 0.001
+                    p = Point()
+                    p.x = d
+                    p.y = (120 - x2) / fx * d
+                    p.z = (90 - v[3]) / fy * d
+                    line_list.points.append(p)
+
+                    lines_pub.publish(line_list)
         else:
             tof.releaseFrame(frame)
 
